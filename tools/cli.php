2e6058df00ff83b2033d78be093f90e76298ece1
annprog <tecbbs@qq.com>
2016-10-25 17:56:36 +0800

    利用ext/get_related导出: 对象,对应APP,对应APP联系人 格式的csv表格

--- tools/cli.php
+++ tools/cli.php
@@ -15,9 +15,10 @@ define('ITOPPWD', $config['itop']['password']);
 
 $iTopAPI = new \iTopApi\iTopClient(ITOPURL, ITOPUSER, ITOPPWD, $version='1.2');
 
-function runOQL($class, $key, $isGet=true) 
+function runOQL($class, $key, $isGet=true, $hide = array(), $depth = "5") 
 {
 	global $iTopAPI;
+	global $config;
 
 	#$key = array("name" => "cmdb");
 	#$key = json_encode($key);
@@ -31,15 +32,75 @@ function runOQL($class, $key, $isGet=true)
 		$data = $iTopAPI->coreGet($class, $key);
 	}else
 	{
-		$data = $iTopAPI->extRelatedPerson($class, $key, 'email,phone,friendlyname', array("BusinessProcess", "ApplicationSolution"));
+		$data = $iTopAPI->extRelatedPerson($class, $key, 'email,phone,friendlyname', $hide, $depth);
 	}
 	return($data);
+}
+
+function export_csv($class, $key)
+{
+	global $config;
+	$depth = "2";
+	if(isset($config['related']['depth']['export_csv']))
+	{
+		$depth = $config['related']['depth']['export_csv'];
+	}
+	$data = json_decode(runOQL($class, $key, false, array(), $depth), true);
+	$persons = $data['objects'];
+	$relations = $data['relations'];
+	$csv_array = array();
+	foreach($relations as $k => $relation)
+	{
+		$obj = explode("::", $k);
+		$obj_type = $obj[0];
+		$obj_value = $obj[2];
+		if($obj_type == "RDS")
+		{
+			$rds = $obj_value;
+			$apps = array();
+			$contacts = array();
+			foreach($relation as $relation_key)
+			{
+				$relation_key_obj  = explode("::", $relation_key['key']);
+				if($relation_key_obj[0] == "ApplicationSolution")
+				{
+					$app = $relation_key_obj[2];
+					array_push($apps, $app);
+					$relation_app_obj = $relations[$relation_key['key']];
+					foreach($relation_app_obj as $relation_app)
+					{
+						$obj_person = explode("::", $relation_app['key']);
+						if($obj_person[0] != "Person")
+						{
+							continue;
+						}
+						$person_key = $obj_person[0] . "::" . $obj_person[1];
+						$person = $persons[$person_key]['fields'];
+						$contact = $person['friendlyname'] . '#' . $person['phone'];
+						array_push($contacts, $contact);
+					}
+				}
+			}
+			$app_str = implode(";", $apps);
+			$contact_str = implode(";", $contacts);
+			$csv = $rds . "," . $app_str . "," . $contact_str;
+			array_push($csv_array, $csv);
+		}	
+	}
+	$csv_str = implode("\n", $csv_array);
+	return($csv_str);
 }
 
 if(isset($argv[1]) and isset($argv[2])){
 	if(isset($argv[3]))
 	{
-		die(runOQL($argv[1], $argv[2], false));
+		if($argv[3] == "csv")
+		{
+			print_r(export_csv($argv[1], $argv[2]));
+		}else
+		{
+			die(runOQL($argv[1], $argv[2], false));
+		}
 	}else
 	{
 		die(runOQL($argv[1], $argv[2]));
