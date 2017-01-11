bd7df447f2b5bbe89fa3b20464afecd5099ce910
annprog <tecbbs@qq.com>
2017-01-11 16:54:20 +0800

    add influxdb client

--- composer/vendor/composer/autoload_static.php
+++ composer/vendor/composer/autoload_static.php
@@ -6,10 +6,34 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit1b805ceb20fade59205b4dd6c5f8d745
 {
+    public static $files = array (
+        'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
+        'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
+        '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
+    );
+
     public static $prefixLengthsPsr4 = array (
         'i' => 
         array (
             'iTopApi\\' => 8,
+        ),
+        'S' => 
+        array (
+            'Symfony\\Component\\EventDispatcher\\' => 34,
+        ),
+        'P' => 
+        array (
+            'Psr\\Http\\Message\\' => 17,
+        ),
+        'I' => 
+        array (
+            'InfluxDB\\' => 9,
+        ),
+        'G' => 
+        array (
+            'GuzzleHttp\\Psr7\\' => 16,
+            'GuzzleHttp\\Promise\\' => 19,
+            'GuzzleHttp\\' => 11,
         ),
     );
 
@@ -17,6 +41,30 @@ class ComposerStaticInit1b805ceb20fade59205b4dd6c5f8d745
         'iTopApi\\' => 
         array (
             0 => __DIR__ . '/..' . '/ec-europa/itopapi/src',
+        ),
+        'Symfony\\Component\\EventDispatcher\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/event-dispatcher',
+        ),
+        'Psr\\Http\\Message\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/http-message/src',
+        ),
+        'InfluxDB\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/influxdb/influxdb-php/src/InfluxDB',
+        ),
+        'GuzzleHttp\\Psr7\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/guzzlehttp/psr7/src',
+        ),
+        'GuzzleHttp\\Promise\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/guzzlehttp/promises/src',
+        ),
+        'GuzzleHttp\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/guzzlehttp/guzzle/src',
         ),
     );
 
