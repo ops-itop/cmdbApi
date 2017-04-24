ce535d1220546a761f8bd3ae788adf5106e6292d
annprog <tecbbs@qq.com>
2017-04-24 12:54:07 +0800

    [actions] fix bug

--- actions/tasks/demo.php
+++ actions/tasks/demo.php
@@ -10,13 +10,13 @@
 
 require dirname(__FILE__).'/../etc/config.php';
 
-$THIS_HOSTNAME = getenv("THIS_HOSTNAME");
-$THIS_NAME = getenv("THIS_NAME");
+$ID = getenv("ID");
+$TITLE = getenv("TITLE");
 
 $log = dirname(__FILE__) . '/../' . $config['tasklogdir'] . "/" .  end(explode("/", $argv[0])) . ".log";
 echo $log;
 sleep(1);
-file_put_contents($log, "$THIS_HOSTNAME $THIS_NAME\n", FILE_APPEND);
+file_put_contents($log, "$ID $TITLE\n", FILE_APPEND);
 ?>
  
  
