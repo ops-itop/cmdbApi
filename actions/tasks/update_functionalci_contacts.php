b144d0cf4b601a20b278587172ecfb3b77a07508
annprog <tecbbs@qq.com>
2017-05-05 14:27:16 +0800

    [actions] update_functionalci_contacts.php更新app，域名，数据库等的contacts字段，update_server_contacts.php更新服务器的contacts字段

--- actions/tasks/update_functionalci_contacts.php
+++ actions/tasks/update_functionalci_contacts.php
@@ -1,8 +1,9 @@
 #!/usr/bin/php
 <?
 /**
- * Usage: 用于更新FunctionalCI的contacts属性。action参数 ID=$this->friendlyname$
- *        也可以单独执行， ID="$hostname" ./update_functionalci_contacts.php 可以批量更新contacts属性
+ * Usage: 用于更新FunctionalCI(除Server外, MiddleWare,Domain等contacts属性取下游App的contacts)的contacts属性。
+ * 触发器作用于lnkContactToApplicationSolution， action参数: ID=>$this->applicationsolution_id$
+ * 也可以单独执行， ID="$id" ./update_functionalci_contacts.php 可以批量更新contacts属性
  * File Name: update_functionalci_contacts.php.php
  * Author: annhe  
  * Mail: i@annhe.net
@@ -16,26 +17,77 @@ $ID = getenv("ID");
 $TITLE = getenv("TITLE");
 $log = dirname(__FILE__) . '/../' . $config['tasklogdir'] . "/" .  end(explode("/", $argv[0])) . ".log";
 
-$oql = "SELECT FunctionalCI AS f WHERE f.friendlyname = '" . $ID . "'";
-$data = $iTopAPI->coreGet("FunctionalCI", $oql, "contacts_list");
-$obj = json_decode($data, true)['objects'];
-if($obj)
+// 取app上游的一级关联，并排除Server和集群,App(上游app的联系人不受下游app影响),机柜等
+$hide_relations = array("Rack", "Server");
+$filter = array("ApplicationSolution");
+$output_fields = array("ApplicationSolution"=>"contact_list_custom"); // 顺便取到app的联系人
+$optional = array("filter"=>$filter,"hide_relations"=>$hide_relations,"depth"=>1, 
+	"direction"=>"up","output_fields"=>$output_fields);
+$data = json_decode($iTopAPI->extRelated("ApplicationSolution", $ID, "impacts", $optional), true);
+
+$result = array();
+// 更新app的联系人
+if($data['objects'])
 {
 	$contacts_arr = array();
-	foreach($obj as $k => $v)
+	$app_contact = $data['objects']["ApplicationSolution::$ID"]['fields']['contact_list_custom'];
+	foreach($app_contact as $k => $person)
 	{
-		foreach($v['fields']['contacts_list'] as $key => $person)
-		{
+		if($person['contact_id_finalclass_recall'] == "Person")
 			$contacts_arr[] = preg_replace('/@.*/s', '', $person['contact_email']);
+	}
+	$result[] = "ApplicationSolution:" . doUpdate($ID,$contacts_arr);
+}else{
+	$result[] = $data;
+}
+
+// 更新上游配置项的联系人
+if($data['relations'])
+{
+	foreach($data['relations'] as $k => $v)
+	{
+		$item = explode("::", $k);
+		$recall = $item[0];
+		$fid = $item[1];
+		// 排除App(上游app的联系人不受下游app影响)
+		if($recall != "ApplicationSolution")
+		{
+			$result[] = $recall . ":" . updateUpstream($fid);
 		}
 	}
+}else{
+	$result[] = $data;	
+}
+
+function updateUpstream($fid)
+{
+	global $iTopAPI;
+	$data = $iTopAPI->extRelated("FunctionalCI", $fid, "impacts", array("depth"=>2));
+	$obj = json_decode($data, true)['objects'];
+	$contacts_arr = array();
+	if($obj)
+	{
+		foreach($obj as $k => $v)
+		{
+			$contacts_arr[] = preg_replace('/@.*/s', '', $v['fields']['email']);	
+		}
+		return(doUpdate($fid, $contacts_arr));
+		
+	}
+	return($data);
+}
+
+function doUpdate($fid,$contacts_arr)
+{
+	global $iTopAPI;
 	$contacts = implode(",", $contacts_arr);
 	$comment = "update contacts to $contacts from action-shell-exec";
-	$ret = $iTopAPI->coreUpdate("FunctionalCI", $oql, array("contacts"=>$contacts),$comment);
-	print_r($ret);
-	$ret = $contacts . " " . json_encode(json_decode($ret, true)['message']);
-} else {
-	$ret = $data;
+	$ret = $iTopAPI->coreUpdate("FunctionalCI", $fid, array("contacts"=>$contacts),$comment);
+	$ret = json_decode($ret, true);
+	$ret = $fid . " - " . $contacts . " code:" . $ret['code'] . " message:" . $ret['message'];
+	return($ret);
 }
+
+$ret = implode(" # ", $result);
 file_put_contents($log, $config['datetime'] . " - $ID - $TITLE - $ret\n", FILE_APPEND);
 ?>
---
+++ actions/tasks/update_server_contacts.php
@@ -0,0 +1,42 @@
+#!/usr/bin/php
+<?
+/**
+ * Usage: 用于更新Server的contacts属性,Server和其他FunctionalCI不一样，是直接取contacts_list作为联系人。触发器作用
+ * 于lnkContactToFunctionalCI, action参数 ID=$this->functionalci_id$
+ * 也可以单独执行， ID="$id" ./update_server_contacts.php 可以批量更新contacts属性
+ * File Name: update_server_contacts.php.php
+ * Author: annhe  
+ * Mail: i@annhe.net
+ * Created Time: 2017-04-24 17:08:30
+ **/
+
+require dirname(__FILE__).'/../etc/config.php';
+
+
+$ID = getenv("ID");
+$TITLE = getenv("TITLE");
+$log = dirname(__FILE__) . '/../' . $config['tasklogdir'] . "/" .  end(explode("/", $argv[0])) . ".log";
+
+$oql = "SELECT Server AS f WHERE f.id = '" . $ID . "'";
+$data = $iTopAPI->coreGet("Server", $oql, "contacts_list");
+$obj = json_decode($data, true)['objects'];
+if($obj)
+{
+	$contacts_arr = array();
+	foreach($obj as $k => $v)
+	{
+		foreach($v['fields']['contacts_list'] as $key => $person)
+		{
+			$contacts_arr[] = preg_replace('/@.*/s', '', $person['contact_email']);
+		}
+	}
+	$contacts = implode(",", $contacts_arr);
+	$comment = "update contacts to $contacts from action-shell-exec";
+	$ret = $iTopAPI->coreUpdate("FunctionalCI", $oql, array("contacts"=>$contacts),$comment);
+	$ret = json_decode($ret, true);
+	$ret = $contacts . " code:" . $ret['code'] . " message:" . $ret['message'];
+} else {
+	$ret = $data;
+}
+file_put_contents($log, $config['datetime'] . " - $ID - $TITLE - $ret\n", FILE_APPEND);
+?>
