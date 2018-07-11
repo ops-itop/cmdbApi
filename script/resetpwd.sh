98b138ed153707994a6436f63a2db1382a26432c
annprog <tecbbs@qq.com>
2018-07-11 12:26:10 +0800

    fix: 用户加入docker组，无需root权限就可以进入容器

--- script/resetpwd.sh
+++ script/resetpwd.sh
@@ -77,7 +77,7 @@ function resetPwd() {
 		cat >>$USER_PP <<EOF
 	user {
 		"$username":
-			ensure => present, home => '/home/$username', managehome => true, groups => ['$username','docker'],
+			ensure => present, home => '/home/$username', managehome => true, groups => ['docker'],
 			password => '$shadowPass';
 	}
 }
