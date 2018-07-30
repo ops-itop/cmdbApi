024ead8b1f29b2a7d105e10688773e505c5818e3
annprog <tecbbs@qq.com>
2018-07-30 13:13:58 +0800

    [script]fix resetpwd.sh

--- script/resetpwd.sh
+++ script/resetpwd.sh
@@ -28,11 +28,10 @@ function sendMail() {
 	user=`echo $email|cut -f1 -d'@'`
 	password=$2
 	gpgKey=$3
-	keyId=`echo "$gpgKey" |sed 's/#/\r\n/g' |gpg --import 2>&1|grep "<"|awk '{print $3}' |sed 's/：/:/g'|awk -F':' '{print $1}'`
+	# 这里也需要设置homedir
+	keyId=`echo "$gpgKey" |sed 's/#/\r\n/g' |gpg --homedir /root/.gnupg --import 2>&1|grep "<"|awk '{print $3}' |sed 's/：/:/g'|awk -F':' '{print $1}'`
 	
 	content="您登陆服务器的用户名为  $user；密码为  $password\r\n\r\n正常情况下1小时内生效，如超过一小时还未生效请联系运维\r\n\r\n如果着急使用，请联系服务器管理员执行puppet同步命令: puppet agent --config /etc/puppet/letv.conf --onetime --verbose --no-daemonize"
-	# 第一次加密总失败，这里等一段时间在加密
-	sleep 10;
 	# cron中使用gpg加密需要设置--homedir
 	content=`echo -e "$content" |gpg --homedir /root/.gnupg --trust-model always --yes --local-user $LOCAL_USER -a -r $keyId --sign -e |sed ':a;N;s/\n/<br>/g;ba'|sed 's/\+/%2B/g'`
 
